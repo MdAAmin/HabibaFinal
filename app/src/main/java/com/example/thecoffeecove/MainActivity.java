2fd487ae418572e8d2fc68f3c8fec79afe3b582e
Habiba <habibabegum992@gmail.com>
2024-09-27 16:50:26 +0600

    second commit

--- app/src/main/java/com/example/thecoffeecove/MainActivity.java
+++ app/src/main/java/com/example/thecoffeecove/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.thecoffeecove;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        LayoutInflater inflater = getLayoutInflater();
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) View layout = inflater.inflate(R.layout.backgroundtoast, findViewById(R.id.go));
+
+        Toast customToast = new Toast(getApplicationContext());
+        customToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        customToast.setView(layout);
+
+        // Find  "Get Started" button
+        Button buttonGetStarted,AdminRegistration;
+        buttonGetStarted = findViewById(R.id.buttonGetStarted);
+        AdminRegistration= findViewById(R.id.button_Admin);
+
+        //  for the button
+        buttonGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // intent for Showing activity
+                customToast.show();
+                Intent intent = new Intent(MainActivity.this, Showing.class);
+                startActivity(intent);
+            }
+        });
+        AdminRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customToast.show();
+                Intent intent = new Intent(MainActivity.this, Admin_Registration_Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
